7bfab106689fdab00547a63971e0c54468cdf485
lucasrodlima <lucasrodlima27@gmail.com>
2025-07-03 12:37:16 -0300

    refactor: add leaf with children unit test

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from textnode import TextNode, TextType
+from leafnode import LeafNode
 
 
 def main():
     node1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    node2 = LeafNode("p", "This is a paragraph.", children=[node1])
 
     print(node1)
+    print(node2)
 
 
 if __name__ == "__main__":
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -17,3 +17,12 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_i(self):
         node = LeafNode("i", "This is italic text")
         self.assertEqual(node.to_html(), "<i>This is italic text</i>")
+
+    def test_leaf_with_children(self):
+        node = LeafNode("i", "This is italic text")
+        with self.assertRaises(TypeError) as e:
+            LeafNode("i", "This is italic text", children=[node])
+        self.assertEqual(
+            str(e.exception),
+            "LeafNode.__init__() got an unexpected keyword argument 'children'",
+        )
